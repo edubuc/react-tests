49dbcd81fe4b4733e8d370fdad98e79ea9d5507e
Emmanuel Dubuc <edubuc@alakees.com>
2016-05-25 17:02:46 +0200

    browserHistory

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Route, Router, IndexRoute, browserHistory} = require('react-router');
 
 var Main = require('Main');
 var Weather = require('Weather');
@@ -15,7 +15,7 @@ $(document).foundation;
 require('style!css!applicationStyles')
 
 ReactDOM.render(
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
 		<Route path="/" component={Main}>
 			<Route path="about" component={About}/>
 			<Route path="examples" component={Examples}/>
